eae3150d257a69cbc1f0510da7e75860a7a30284
trrrrrys <tsukahara-ryo@dmm.com>
2022-10-28 14:12:08 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trrrrrys/gopp
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+)
+
+var out = os.Stdout
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(out, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+var (
+	current string
+	srcPath string
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+	flag.StringVar(&current, "c", ".", "go package path")
+	flag.StringVar(&srcPath, "s", "$GOPATH/src", "go source Path")
+}
+
+func run() error {
+	flag.Parse()
+	u, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	ac, err := filepath.Abs(strings.Replace(os.ExpandEnv(current), "~", u.HomeDir, 1))
+	if err != nil {
+		return err
+	}
+	as, err := filepath.Abs(strings.Replace(os.ExpandEnv(srcPath), "~", u.HomeDir, 1))
+	if err != nil {
+		return err
+	}
+	packageName := strings.Replace(ac, as+"/", "", 1)
+	if strings.HasPrefix(packageName, "/") {
+		return fmt.Errorf("invalid input, go package Path %s, gosrc path %s", ac, as)
+	}
+	fmt.Fprint(out, packageName)
+	return nil
+}
